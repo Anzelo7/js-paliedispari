5c26a45c435ef0c9e138ee09b10e50c861eebc97
Anzelo7 <andrea.anzellotti7@gmail.com>
2022-09-04 18:22:00 +0200

    push

--- script.js
+++ script.js
@@ -1,47 +1,102 @@
-console.log('JS OK')
+console.log('JS OK!');
 
-//scegliere numero pari o dispari
+///////////////////////////////// Palindroma
 
-const EvenOdd = parseInt(prompt('Scegli pari o dispari:'));
+// Chiedere all’utente di inserire una parola
 
-let pari = 'pari'
-let dispari = 'dispari'
+const word = prompt('Scrivi una parola');
+
+const isPalindroma = checkPalindroma(word);
+
+console.log('è palindroma:', isPalindroma)
+
+// funzione per capire se la parola inserita è palindroma
+
+// ciclo i caratteri in ordine e aggiungo il carattere in testa a reversedText
+function checkPalindroma(text) {
+    // NB: palindroma = la parola è sempre la stessa sia che sia letta da sx a dx cghe da dx a sx
+    let reversedText = '';
+    // ciclo i caratteri di text
+    console.log('checkPalindroma');
+
+    for (let i = 0; i < text.length; i++) {
+        const char = text.charAt(i);
+        // aggiungo a una stringa vuota i caratteri in ordine invertito
+        reversedText = char + reversedText; // aggiunto in testa , reversedText = reversedText + char
+        console.log(reversedText);
+    }
+
+    console.log('reversedText', reversedText);
+    // controllo se il testo risultante è uguale a quello di input
+    const isEqual = reversedText === text;
+    // ritorno true se palindroma altrimenti false
+    return isEqual;
+
+}
+
+function reverseString(textToReverse) {
+
+    const result = textToReverse.split('').reverse().join('');
 
 
-if (pari) {
-    document.getElementById('numberUtente').innerHTML = 'pari'
-} else if (dispari) {
-    document.getElementById('numberUtente').innerHTML = 'dispari'
+    return result;
+}
+
+
+//////////////////////////////////////////// Pari e Dispari
+
+// chiedo di scegliere pari o dispari
+
+let userChoice = '';
+
+while (userChoice !== 'pari' && userChoice !== 'dispari') { // per ciclare deve essere true
+    userChoice = prompt('Pari o dispari?');
+    if (userChoice !== undefined) {
+        userChoice = userChoice.toLowerCase();
+    }
+}
+
+// chiedo all'utente un numero tra 1 e 5
+
+let number = 0;
+
+while (isNaN(number) || number < 1 || number > 5) {
+    number = parseInt(prompt('dammi un numero tra 1 e 5'));
+}
+
+
+// generiamo un numero random 
+const numberPC = getRandomNumber(1, 5);
+
+// sommiamo i 2 numeri
+
+const sum = number + numberPC;
+
+
+// verifichiamo se la somma è pari o dispari
+const pariODispari = isOddOrEven(sum) ? 'pari' : 'dispari';;
+
+
+// dichiariamo chi ha vinto (userChoice è la scelta dell'utente)
+if (userChoice === pariODispari) {
+    console.log(userChoice, sum, 'hai vinto!');
 } else {
-    alert(promt("Inserisci pari o dispari"));
+    console.log(userChoice, sum, 'hai perso!');
 }
 
 
-//inserire numero da 1 a 5
 
-const utente = parseInt(prompt('Scegli numero da 1 a 5:'));
 
-document.getElementById('numberUtente').innerHTML = utente
+function getRandomNumber(min, max) {
+    const range = max - min + 1
 
-//computer sceglie random n da 1/5
+    const random = Math.floor(Math.random() * range) + min;
 
-let computer = Math.floor(Math.random() * 4) + 1;;
+    return random;
 
-const numberPC = []
-
-function addNumber(numberPC) {
-    while (numberPC.length < 5)
-
-        return numberPC = Math.floor(Math.random() * 4) + 1;;
 }
-console.log(addNumber(numberPC));
-document.getElementById('numberPC').innerHTML = numberPC
 
-//somma dei numeri pari o dispari
-let sum = utente + numberPC
-//winner
+function isOddOrEven(numberToCheck) {
+    return (numberToCheck % 2 === 0)
+}
 
-if (sum % 2 === 0) {
-    document.getElementById('numberUtente').innerHTML = 'Winner'
-} else
-    document.getElementById('numberPC').innerHTML = 'Winner'
